e54e5ebcbfb3cad99cedee2f715b12b5677b8314
Bhagyashri <bhagyashrideoghare116@gmail.com>
2024-06-28 12:42:23 +0530

    project setup and routing

---
+++ src/pages/blog/Blog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Blog() {
+  return (
+    <div className='bg-black'>Blog</div>
+  )
+}
+
+export default Blog
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+     <Button>Home</Button>
+    </div>
+  )
+}
+
+export default Home;
